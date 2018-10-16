4a56578d4ec601be0eec18589fb778fea35a09cf
Christopher Milligan <milliganjchristopher@gmail.com>
2018-10-16 16:59:08 +0100

    volume of sphere working

--- start_point/specs/ruby_functions_spec.rb
+++ start_point/specs/ruby_functions_spec.rb
@@ -78,11 +78,12 @@ class FunctionsTest < MiniTest::Test
     assert_equal(8, test_cube)
   end
 
-  # #Given the radius of a sphere calculate the volume
-  # def test_volume_of_sphere()
-  #   #add test code here
-  # end
-  #
+  #Given the radius of a sphere calculate the volume
+  def test_volume_of_sphere()
+    test_sphere = volume_of_sphere(8)
+    assert_equal(2144.66, test_sphere)
+  end
+
   # #Given a value in farenheit, convert this into celsius.
   # def test_fahrenheit_to_celsius()
   #   #add test code here
